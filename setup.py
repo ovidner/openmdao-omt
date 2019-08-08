2b288ebcbc0b8cace6bb854a74cb1c0d31143798
Olle Vidner <olle@vidner.se>
2019-08-08 17:19:36 +0200

    Initial commit 🎺

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="openmdao-utils",
+    version="0.0.0",
+    author="Olle Vidner",
+    author_email="olle@vidner.se",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.7, <4",
+    install_requires=["openmdao"],
+)
